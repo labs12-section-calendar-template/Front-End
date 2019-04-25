c23ea26d94bd17cbdee11fe758e3d65bb6d9ba24
ills5771 <ilyayelizarov1@gmail.com>
2019-04-24 18:00:59 -0700

    replaces Auth in return of App.js with MainView

--- calendr/src/App.js
+++ calendr/src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
-import { withRouter } from 'react-router-dom';
-import LoginView from './components/views/loginView';
-import mainView from './components/views/mainView';
-import Authenticate from './components/login/Authentication';
-
+import React from "react";
+import "./App.css";
+import { withRouter } from "react-router-dom";
+import LoginView from "./components/views/loginView";
+import MainView from "./components/views/mainView";
+import Authenticate from "./components/login/Authentication";
 
 // import Month from './components/calendar/Month'
 // import Day from './components/calendar/Day'
 
 function App() {
   return (
-    <Auth/>
+    // <Auth/>
+    <MainView />
   );
 }
 
-
-const Auth = withRouter(Authenticate(mainView)(LoginView));
+// const Auth = withRouter(Authenticate(mainView)(LoginView));
 
 export default withRouter(App);
