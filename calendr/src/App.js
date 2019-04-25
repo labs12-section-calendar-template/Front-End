8300ec368da148a559fa74f1ce6337be5d1e39fa
ills5771 <ilyayelizarov1@gmail.com>
2019-04-24 17:14:51 -0700

    adds axios

--- calendr/src/App.js
+++ calendr/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/general/NavBar'
-import MarketingPage from './components/general/MarketingPage'
+import React from "react";
+import "./App.css";
+import NavBar from "./components/general/NavBar";
+import MarketingPage from "./components/general/MarketingPage";
 // import Month from './components/calendar/Month'
 // import Day from './components/calendar/Day'
+import Week from "./components/calendar/Week";
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
-     <MarketingPage/>
-     {/* <Month/>
+      <NavBar />
+      {/* <MarketingPage/> */}
+      {/* <Month/>
      <Day/> */}
+      <Week />
     </div>
   );
 }
--- calendr/src/components/calendar/Week.js
+++ calendr/src/components/calendar/Week.js
@@ -1,13 +1,56 @@
-import React, { Component } from 'react'
+// import React, { Component } from "react";
+// import dateFns from "date-fns";
 
-export class Week extends Component {
+// export class Week extends Component {
+//   state = {
+//     week: new Date()
+//   };
+
+//   showWeekdays = () => {
+//     const format = "dd";
+//     const days = [];
+//     let startWeek = dateFns.startOfWeek(this.state.week);
+
+//     for (let i = 0; i < 7; i++) {
+//       startWeek.push(
+//         <div>{dateFns.format(dateFns.addDays(startWeek, i), format)}</div>
+//       );
+//     }
+//     return <div>{days}</div>;
+//   };
+
+//   render() {
+//     return <div>{this.showWeekdays()}</div>;
+//   }
+// }
+import React, { Component, Fragment } from "react";
+
+import dateFns from "date-fns";
+
+class Week extends Component {
+  state = {
+    week: new Date()
+  };
+
+  showDays = () => {
+    const format = "dd";
+    const days = [];
+    let startDate = dateFns.startOfWeek(this.state.week);
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div>{dateFns.format(dateFns.addDays(startDate, i), format)}</div>
+      );
+    }
+    return <div>{days}</div>;
+  };
+
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <Fragment>
+        <div className="days">{this.showDays()}</div>
+      </Fragment>
+    );
   }
 }
 
-export default Week
+export default Week;
