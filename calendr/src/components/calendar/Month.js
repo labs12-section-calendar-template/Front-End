724f5331c7f36565de4d24f28e65d0740c55997b
Max Trestman <mtrestman1@Maxs-MacBook-Pro.local>
2019-04-25 17:23:48 -0400

    state in Month and previous function built with dateFns

--- calendr/src/components/calendar/Month.js
+++ calendr/src/components/calendar/Month.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
-// import {format, compareAsc} from 'date-fns/esm'
-import startOfMonth from 'date-fns/start_of_month'
-import endOfMonth from 'date-fns/end_of_month'
-import addMonth from 'date-fns/add_months'
-import subMonth from 'date-fns/sub_months'
-import addWeek from 'date-fns/add_weeks'
+import { format, addMonths, subMonths } from 'date-fns'
 
 export class month extends Component {
     state = {
-        month: new Date()
+        month: format(new Date(), "MM/DD/YYYY")
     }
 
     // componentDidMount() {
@@ -18,31 +13,22 @@ export class month extends Component {
     // OnClick this function moves to the previous month
 previous = () => {
         const { month } = this.state;
-        // console.log(month)
+        console.log(month)
         this.setState({
-          month: subMonth(month, 1)
+          month: format(subMonths(month, 1), "MM/DD/YYYY")
         });
       };
         
-    // OnClick this function moves to the next month
-next = () => {
-        const { month } = this.state;
-        // console.log(month)
-        this.setState({
-          month: addMonth(month, 1)
-        });
-      };
     
 
   render() {
     let { month } = this.state
-    
     return (
+        // Uncomment buttons and console.logs to try the month increment and decrement 
       <div>
-        {/* Uncomment buttons and console.logs to try the month increment and decrement 
+        <button onClick={this.previous}> previous </button>
         <button onClick={this.next}> next </button>
-        <button onClick={this.previous}> previous </button> */}
-
+      
 
       </div>
     )
