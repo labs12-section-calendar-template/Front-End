cfa41e5a4201e7e566fa5d4f86e84d26d9daa7d1
ills5771 <ilyayelizarov1@gmail.com>
2019-04-26 08:38:47 -0700

    adds _redirects file to display routes on netlify

--- calendr/src/components/calendar/Week.js
+++ calendr/src/components/calendar/Week.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
 
-export class Week extends Component {
+import dateFns from "date-fns";
+
+class Week extends Component {
+  state = {
+    week: new Date()
+  };
+
+  weekDays = () => {
+    const days = [];
+    let startDate = dateFns.startOfWeek(this.state.week);
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div>{dateFns.format(dateFns.addDays(startDate, i), "dd")}</div>
+      );
+    }
+    return days.map(day => <div>{day}</div>);
+  };
+
   render() {
     return (
       <div>
-        
+        <div className="days">{this.weekDays()}</div>
       </div>
-    )
+    );
   }
 }
 
-export default Week
+export default Week;
