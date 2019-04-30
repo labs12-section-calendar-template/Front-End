07a43cb0e7c6ffc9787053bd9b41bcfd2751c598
Max Trestman <mtrestman1@gmail.com>
2019-04-30 16:20:43 -0700

    Template creation component without styling

--- calendr/src/components/homePage/template/Template.js
+++ calendr/src/components/homePage/template/Template.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react'
+import SideBar from '../SideBar';
+import GeneralCalendar from '../../calendar/GeneralCalendar';
 
 export class Template extends Component {
+  constructor(props) {
+    super(props);
+    
+this.state = {
+    title: '',
+    description: '',
+    value: ''
+    }
+}
+
+handleChange = event => {
+  this.setState({
+    value: event.target.value
+  });
+}
+
+handleInputChange = event => {
+  this.setState({
+      [event.target.name]: event.target.value
+  })
+}
+
+
   render() {
     return (
       <div>
+        <SideBar /> 
         <h1>Template Creation</h1>
         <button>Save</button>
-
+        <h3>Cycle Length: {' '}
+                <select value={this.state.value} onChange={this.handleChange}>
+                    <option value='four'>4 Weeks</option>
+                    <option value='five'>5 Weeks</option>
+                    <option value='six'>6 Weeks</option>
+                    </select>
+                </h3>
+                <form>
+               <li> <input 
+                onChange={this.handleInputChange}
+                placeholder="Title"
+                value={this.state.title}
+                name="title" 
+                /> </li>
+               <li> <input 
+                onChange={this.handleInputChange}
+                placeholder="Description"
+                value={this.state.description}
+                name="description"
+                /> </li>
+            </form>
+      <GeneralCalendar />
       </div>
     )
   }
