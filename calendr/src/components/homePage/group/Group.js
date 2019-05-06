40be25841b4a494bddb6f5b8b3b2d56fc9d83c21
Jacob McFaul <mcjakedude@gmail.com>
2019-05-06 09:57:45 -0700

    minor updates

--- calendr/src/components/homePage/group/Group.js
+++ calendr/src/components/homePage/group/Group.js
@@ -11,17 +11,16 @@ export class Group extends Component {
     
     postGroup = e => {
       e.preventDefault();
-      const createdCode = this.state.joinCode
-      let { name } = this.state
+      let { name, joinCode } = this.state
       axios
-        .post(`https://calendr.netlify.com/users/:id/groups`, { name, createdCode })
+        .post(`https://calendr.netlify.com/users/:id/groups`, { name, joinCode })
         .then(res => {
           console.log(res.data);
           this.setState({
-            createdCode: '',
+            joinCode: '',
             name: '',
           });
-          if(this.state.createdCode !== null && this.state.name !== null){
+          if(this.state.joinCode !== null && this.state.name !== null){
             this.props.history.push('/home')
           }else{
             alert('Fill out all fields')
@@ -59,8 +58,8 @@ handleInputChange = event => {
             className="groupInput"
             onChange={this.handleInputChange}
             placeholder="Join code..."
-            value={this.state.createdCode}
-            name="createdCode"
+            value={this.state.joinCode}
+            name="joinCode"
             type="number"
             />
             <button className="formButton">Create</button>
