9eadcf3f41f163fabaf721eb2e6084d84e78a2d9
Max Trestman <mtrestman1@gmail.com>
2019-05-15 15:13:27 -0400

    ability to display multiple groups, change views, templates, routing etc

--- calendr/src/components/homePage/SideBar.js
+++ calendr/src/components/homePage/SideBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import Popup from 'reactjs-popup';
 import GroupEdit from './group/GroupEdit'
+import { withRouter } from 'react-router-dom';
 
 export class SideBar extends Component {
   constructor(props){
@@ -10,34 +11,58 @@ export class SideBar extends Component {
       groupName: [],
       joinCode: [],
       group_id:[],
-      modalOpen: false
+      modalOpen: false,
+      groups: []
     }
   }
 
   componentDidMount(){
     this.getGroup();
+    
     // if(this.state.groupName.length < 0){
     //   window.location = '/'
     // }else{
     //   window.location = '/home'
     // }
   }
+
+  componentDidUpdate = (prevProps) => {
+    if(prevProps.match.url !== this.props.match.url) {
+      this.getGroup()
+    }
+  }
   
   getGroup = () => {
     let userId = localStorage.getItem('userId')
+    let groupId = localStorage.getItem("group_id")
     axios.get(`${process.env.REACT_APP_API}/users/${userId}/groups`)
     .then(res => {
       this.setState({
-        group_id:res.data[0].id,
-        groupName: res.data[0].name,
-        joinCode: res.data[0].joinCode,
+        group_id: groupId,
+        groups: res.data
       })
-      console.log(this.state.group_id)
-      window.localStorage.setItem("group_id", this.state.group_id)
+      this.getGroupById(groupId)
+      console.log(groupId)
+      console.log(res.data)
+     // window.localStorage.setItem("group_id", this.state.group_id)
     })
     .catch(err => {
       console.log(err)
     })
+  }
+
+  getGroupById = (something) => {
+    
+    axios.get(`${process.env.REACT_APP_API}/groups/${something}`)
+    .then(res => {
+      this.setState({
+        groupName: res.data.name,
+        joinCode: res.data.joinCode,
+      })
+  })
+  .catch(err => {
+    console.log(err)
+  })
   }
 
   circleAddTemplate = () => {
@@ -60,24 +85,40 @@ export class SideBar extends Component {
     }
   }
 
+  something = (event) => {
+    let groups = this.state.groups
+    groups.forEach(group => {
+      console.log(event.target.attributes.getNamedItem('value').value)
+    if(event.target.attributes.getNamedItem('value').value == group.id) {
+      window.localStorage.setItem("group_id", event.target.attributes.getNamedItem('value').value)
+    this.props.history.push(`/home/${group.id}`)
+ 
+    } 
+  })
+   
+  }
+
   render() {
-    // console.log(localStorage)
     return (
       <>
         <div className="homePageStyles">
         <div className="groupNameTemplate">
           <h2 className="GroupName">{this.state.groupName}</h2>
           <i className="far fa-edit" onClick={this.toggleModal}/>
+       
+          <p className='buttonDescriptions'>Join Code {this.state.joinCode}</p>
         </div>
-          <h5 className='buttonTitles'>Group Members</h5>
-        <div className='buttonBox'>
-        <i className="fas fa-plus-circle" />
-          <p className='buttonDescriptions'>Invite to groups<br/>Join Code {this.state.joinCode}</p>
+        <div>
+        <h5 className='buttonTitles'>Groups</h5>
+       {this.state.groups.map(group => (
+      <div key={group.id} value={group.id} className="groupName" onClick={this.something} >
+    {group.name}
+      </div>
+        ))}
         </div>
-        <div className='buttonBox'>
-        <i className="fas fa-plus-circle" onClick={this.removeUsers}/>
-          <p className='buttonDescriptions'>Remove Members</p>
-        </div>
+          
+       
+       
             <h5 className='buttonTitles'>Templates</h5>
         <div className='buttonBox'>
         <i className="fas fa-plus-circle" onClick={this.circleAddTemplate}/>
@@ -92,4 +133,4 @@ export class SideBar extends Component {
   }
 }
 
-export default SideBar
+export default withRouter(SideBar)
