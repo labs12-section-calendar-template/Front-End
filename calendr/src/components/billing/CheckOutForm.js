d590849211c34e68a2784b56ad15ea45cff98090
Max Trestman <mtrestman1@gmail.com>
2019-05-23 13:10:42 -0400

    fixed conflict again

--- calendr/src/components/billing/CheckOutForm.js
+++ calendr/src/components/billing/CheckOutForm.js
@@ -55,7 +55,7 @@ class CheckOutForm extends React.Component {
     render() { 
       let groupId = localStorage.getItem('group_id')
       if(this.state.premiumStatus == true) {
-        return <h2>YOU HAVE ALREADY PAID FOR PREMIUM MEMBERSHIP</h2>
+        return <h1>YOU HAVE ALREADY PAID FOR PREMIUM MEMBERSHIP</h1>
       } else {
       if (this.state.complete) return <div><h1>Purchase Complete</h1> <button onClick={()=> { window.location=`/home/${groupId}`}}>Take Me Home</button> </div>
         return ( 
